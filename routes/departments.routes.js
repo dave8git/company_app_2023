582557e6171175efac4bb976f4f2f2502ff6fa04
dave8git <dave8git@gmail.com>
2023-01-26 12:59:32 +0100

    Add response summary

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -67,11 +67,11 @@ router.put('/departments/:id', async (req, res) => {
     if (dep) {
       await Department.updateOne(
         { _id: req.params.id },
-        { $set: { name: name } }
+        { $set: { ...req.body } }
       );
       res.json({
         message: 'OK',
-        modifiedDepartment: await Department.findById(req.params.id),
+        modified: dep,
       });
     } else res.status(404).json({ message: 'Not found...' });
   } catch (err) {
@@ -84,7 +84,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK'});
+      res.json({ message: 'OK', deleted: dep});
     }
     else res.status(404).json({ message: 'Not found...'});
   }
